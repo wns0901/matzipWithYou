761dbb090ab22b26ddc4462ef80a3e6329063d5a
wns0901 <wnswns012@gmail.com>
2024-12-02 16:42:51 +0900

    Feat(#11): MatzipService 메소드 생성

--- src/main/java/com/lec/spring/matzip/service/MatzipService.java
+++ src/main/java/com/lec/spring/matzip/service/MatzipService.java
@@ -1,4 +1,14 @@
 package com.lec.spring.matzip.service;
 
+import com.lec.spring.matzip.domain.Matzip;
+
 public interface MatzipService {
+    int saveMatzip(Matzip matzip);
+
+    String getImgUrlFromKakao(Long kakaoPlaceId);
+
+    int deleteMatzip(Long id);
+
+    int findById(Long id);
+
 }
