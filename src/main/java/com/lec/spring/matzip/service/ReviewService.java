f946c4e5dcaaa744750470e67fe60a6dab8f13f1
solsol-815 <solsolsol815815@gmail.com>
2024-12-02 16:23:21 +0900

    Feat(#16): Review Service 수정

--- src/main/java/com/lec/spring/matzip/service/ReviewService.java
+++ src/main/java/com/lec/spring/matzip/service/ReviewService.java
@@ -1,7 +1,6 @@
 package com.lec.spring.matzip.service;
 
 import com.lec.spring.matzip.domain.FoodKind;
-import com.lec.spring.matzip.domain.Matzip;
 import com.lec.spring.matzip.domain.Review;
 import com.lec.spring.matzip.domain.Tag;
 import com.lec.spring.member.domain.Member;
