5ab3cd21e22da0e0405b13e719ebfa14c1ad01d0
wns0901 <wnswns012@gmail.com>
2024-12-02 10:24:50 +0900

    Feat(#11): Matzip 도메인 생성

--- src/main/java/com/lec/spring/matzip/domain/Matzip.java
+++ src/main/java/com/lec/spring/matzip/domain/Matzip.java
@@ -1,4 +1,22 @@
 package com.lec.spring.matzip.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Matzip {
+    Long id;
+    String name;
+    Long kindId;
+    String address;
+    Double lat;
+    Double lng;
+    String imgUrl;
+    String gu;
+    String kakaoMapUrl;
 }
