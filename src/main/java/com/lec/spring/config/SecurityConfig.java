d9d2c21ac44e3b2dd82020a74f5f92e2fed8c303
Donghee Lee <dhleedev47@gmail.com>
2024-12-05 15:22:43 +0900

    Feat(#28): validation 진행

--- src/main/java/com/lec/spring/config/SecurityConfig.java
+++ src/main/java/com/lec/spring/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -42,7 +41,7 @@ public class SecurityConfig {
                         .loginPage("/member/login")
                         .loginProcessingUrl("/member/login")
                         .defaultSuccessUrl("/")
-                        .successHandler(new CustomLoginSuccessHandler("/index"))
+                        .successHandler(new CustomLoginSuccessHandler("/home"))
                         .failureHandler(new CustomLoginFailureHandler())
                 )
 
