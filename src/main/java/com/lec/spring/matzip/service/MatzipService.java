514eae1e687d4dd9e03256fd3170cf71d7d74ebd
Donghee Lee <dhleedev47@gmail.com>
2024-12-09 21:28:08 +0900

    Merge(): merge issue solved

--- src/main/java/com/lec/spring/matzip/service/MatzipService.java
+++
@@ -1,9 +0,0 @@
-package com.lec.spring.matzip.service;
-
-import com.lec.spring.matzip.domain.Matzip;
-
-public interface MatzipService {
-    int saveMatzip(Matzip matzip, String kind);
-
-    String getImgUrlFromKakao(String kakaoPageUrl);
-}
