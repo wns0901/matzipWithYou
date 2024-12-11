91ee936a9cb068715eb03e1d1254558d9b10f479
solsol-815 <solsolsol815815@gmail.com>
2024-12-11 16:16:15 +0900

    Fix(): dev

--- src/main/java/com/lec/spring/matzip/repository/ReviewRepository.java
+++ src/main/java/com/lec/spring/matzip/repository/ReviewRepository.java
@@ -2,10 +2,12 @@ package com.lec.spring.matzip.repository;
 
 import com.lec.spring.matzip.domain.*;
 import com.lec.spring.member.domain.Member;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 
 import java.util.List;
 
+@Repository
 public interface ReviewRepository {
     List<Review> findAll();
 
