566d1a2fd481db2612737df93c862d60201b7151
solsol-815 <solsolsol815815@gmail.com>
2024-12-11 12:37:05 +0900

    Feat(#47): friend updateIntimacy repository 및 xml 수정

--- src/main/java/com/lec/spring/member/repository/FriendRepository.java
+++ src/main/java/com/lec/spring/member/repository/FriendRepository.java
@@ -1,6 +1,8 @@
 package com.lec.spring.member.repository;
 
 import com.lec.spring.member.domain.Friend;
+import com.lec.spring.member.domain.FriendDetailsDTO;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,28 +10,28 @@ import java.util.List;
 @Repository
 public interface FriendRepository{
 
-    // id 로 신청 받은 유저 리턴
-    List<Friend> findByReceiver(Long receiverId);
+    // 친구 신청시 이미 보낸 신청은 없나 확인용
+    boolean isAlreadyFriend(Friend friend);
 
-    // id로 내가 보낸 신청 리턴
-    List<Friend> findBySender(Long senderId);
-
-    // 친구 목록 조회 (수락된 상태만)
-    List<Friend> findFriends(Long userId);
+    // 친구 목록 조회 (친구와 맛집 공개 상태 정보 포함)
+    List<Friend> findFriendsWithDetailsDTO(Long memberId);
 
     // 대기 중인 요청 목록 조회
-    List<Friend> findPendingRequests(Long userId);
+    List<Friend> findPendingRequests(Long memberId);
 
+    // 친구 요청 수락
+    int acceptFriendRequest(Friend friend);
 
-
-
+    // 친구 요청 거절 & 친삭
+    int rejectFriendRequest(Friend friend);
 
     // 친구 요청 보내기
-    int sendFriendRequest(Long receiverId, Long senderId);
+    int sendFriendRequest(Friend friend);
 
-    // 새로운 친구 DB에 저장
+    // 친구 요청 DB에 저장
     int save(Friend friend);
 
-    // 친구 삭제
-    int delete(Friend friend);
+    // 친밀도 업데이트
+    int updateIntimacy(@Param("memberId") Long memberId, @Param("intimacy")Integer intimacy);
+
 }
