5de988badb16bef4f052e4b8bf5c361012cff800
Irenda-Lee <yeonsuhan97@gmail.com>
2024-12-11 15:08:09 +0900

    Feat(#32): mypage Repo 수정

--- src/main/java/com/lec/spring/member/repository/MyPageRepository.java
+++ src/main/java/com/lec/spring/member/repository/MyPageRepository.java
@@ -1,14 +1,27 @@
 package com.lec.spring.member.repository;
 
+import com.lec.spring.member.domain.MyPage;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MyPageRepository {
 
-    int myProfile();
+    // 닉, 친구수, 포인트
+    MyPage myProfile();
 
-    int myActsCount();
+    // 위시리스트 & 내맛집 & 내리뷰 개수
+    int myActsCount(MyPage mypage);
 
+    // 위시 미리보기
+    String wishContents();
+
+    // 내맛집 미리보기
+    String myMatzipContents();
+
+    // 내리뷰 미리보기
+    String myReviewContents();
+
+    // 닉네임 수정
     int updateNick();
 
 
