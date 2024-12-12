34249e10adcae286c5db4281858ece4f8f419e8c
wns0901 <wnswns012@gmail.com>
2024-12-12 12:39:50 +0900

    Feat(#107): wishList 레파지토리 구현 완료

---
+++ src/main/java/com/lec/spring/matzip/repository/WishListRepository.java
@@ -0,0 +1,14 @@
+package com.lec.spring.matzip.repository;
+
+import com.lec.spring.matzip.domain.DTO.WishListDTO;
+import com.lec.spring.matzip.domain.WishList;
+
+import java.util.List;
+
+public interface WishListRepository {
+    boolean save(WishList wishList);
+
+    List<WishListDTO> getByMemberId(long id);
+
+    boolean delete(WishList wishList);
+}
