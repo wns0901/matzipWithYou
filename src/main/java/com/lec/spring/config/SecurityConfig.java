2f3d3a6a416cc9c1af2f36c6dfe62967a090bec4
Donghee Lee <dhleedev47@gmail.com>
2024-12-17 16:56:53 +0900

    Feat(): security 권한 수정

--- src/main/java/com/lec/spring/config/SecurityConfig.java
+++ src/main/java/com/lec/spring/config/SecurityConfig.java
@@ -74,6 +74,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/matzips/**",
                                 "/member/**",
+                                "/member/{id}",
                                 "/member/*/friends",
                                 "/matzips/reviews",
                                 "/matzips/hints/**",
