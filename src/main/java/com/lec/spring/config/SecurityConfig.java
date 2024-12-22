2dc4a8216544e0fc8d0b809704e8f0528754c3dd
Donghee Lee <dhleedev47@gmail.com>
2024-12-22 22:42:52 +0900

    Feat(): mypage 접근권한 변경

--- src/main/java/com/lec/spring/config/SecurityConfig.java
+++ src/main/java/com/lec/spring/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 @Configuration
 @EnableWebSecurity
@@ -39,6 +40,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
+                .addFilterBefore(new MemberAccessFilter(), FilterSecurityInterceptor.class)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/",
@@ -55,7 +57,6 @@ public class SecurityConfig {
                         ).permitAll()
 
                         .requestMatchers(
-//                                "/members/{id}",
                                 "/matzips/food-kinds/**",
                                 "/matzips/tags/**",
                                 "/admin/**"
