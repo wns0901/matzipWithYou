5bb041404ea11e3771944d6f1054d0ef1fd15765
wns0901 <wnswns012@gmail.com>
2024-12-24 02:40:46 +0900

    Feat(): 크롬드라이버 설정

--- src/main/java/com/lec/spring/matzip/service/MatzipServiceImpl.java
+++ src/main/java/com/lec/spring/matzip/service/MatzipServiceImpl.java
@@ -70,7 +70,11 @@ public class MatzipServiceImpl implements MatzipService {
     public String getImgUrlFromKakao(String kakaoPageUrl) {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
-        options.addArguments("window-size=1400,1500");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("disable-infobars"); // 정보 표시 비활성화
+        options.addArguments("--disable-gpu");
+        options.addArguments("window-size=1920,1080"); // 창 크기 설정
         options.setBinary("/usr/bin/google-chrome");
         WebDriver driver = new ChromeDriver(options);
         driver.get(kakaoPageUrl);
