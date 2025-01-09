fbb361fc792cb7d5fc42e32570a91cae15ca9f75
Xuswnss <cooconut0226@gmail.com>
2025-01-09 15:28:51 +0900

    Fix(#182) : wish-list 프로필 수정

--- src/main/resources/static/JS/matzip/wish-list.js
+++ src/main/resources/static/JS/matzip/wish-list.js
@@ -5,12 +5,33 @@ document.addEventListener('DOMContentLoaded', async () => {
     displayProfile()
 });
 
+// function displayProfile(data) {
+//     const friendProfile = document.querySelector('.friend-profile');
+//  const imgUrl = profile && profile.filename
+//      ? '/upload/' + profile.filename :
+//       '/static/IMG/matzip/defaultProfileImg.png';
+//  console.log(imgUrl)
+//
+//
+//
+//     friendProfile.innerHTML = `
+//        <img src="${imgUrl}" >
+//    `;
+// }
+
 function displayProfile(data) {
     const friendProfile = document.querySelector('.friend-profile');
-    const imgUrl = data.profile
-        ? `/upload/${data.profile.filename}?t=${new Date().getTime()}`
-        : `/upload/defaultProfileImg.png?t=${new Date().getTime()}`;
+    let imgUrl;
 
+    if (data && data.filename) {
+        // 프로필 데이터가 있고, 파일명이 존재하는 경우
+        imgUrl = '/upload/' + data.filename;
+    } else {
+        // 기본 프로필 이미지를 사용하는 경우
+        imgUrl = '/IMG/defaultProfileImg.png';
+    }
+
+    console.log(imgUrl); // 디버깅용 로그
 
     friendProfile.innerHTML = `
        <img src="${imgUrl}" alt="프로필 이미지">
